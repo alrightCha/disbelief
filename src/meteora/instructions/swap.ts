31c1c0864c8838fe06e554eecfd664dcdea48ceb
alrightCha <ychamam7@gmail.com>
2025-05-19 17:15:58 +0200

    removed logging

--- src/meteora/instructions/swap.ts
+++ src/meteora/instructions/swap.ts
@@ -41,8 +41,6 @@ export const getSwapIx = async (
   
   while(virtualPoolState == null){
     const receivedState = await client.state.getPool(pool);
-    console.log("Migration progress: ", receivedState.migrationProgress); 
-    console.log("POOL SOL: ", receivedState.baseReserve); 
     if(receivedState != null){
       virtualPoolState = receivedState; 
     }else{
