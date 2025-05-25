739fa302baa29f71970ac1c97bfe9cbf76984241
alrightCha <ychamam7@gmail.com>
2025-05-25 02:11:12 +0200

    back at using many gateways + mine too now

--- src/solana/utils.ts
+++ src/solana/utils.ts
@@ -63,15 +63,44 @@ export async function getTweetMetadataFromIpfs(
   uri: string,
   cid: string
 ): Promise<TweetMetadata> {
-  const prefix = "http://127.0.0.1:8080/ipfs";
-  const endpoint = `${prefix}/${cid}`;
+  const everlandPath = everland(cid);
+  const dwebPath = dweb(cid);
+  const classicPath = basicIpfs(cid);
+  const pinataPath = pinataIpfs(cid);
+  const infuraPath = infura(cid);
+  const nftStoragePath = nftStorage(cid);
+  const web3Path = web3Storage(cid);
+  const fleekPath = ipfsFleek(cid);
+  const thirdPath = ipfsThirdweb(cid);
 
-  const res = await fetch(endpoint);
-  if (!res.ok)
-    throw new Error(
-      `IPFS fetch failed: ${res.status} ${res.statusText} for ${endpoint}`
-    );
-  const json: PeekJson = await res.json();
-  const { tweetId, tweetCreatorUserId, tweetCreatorUsername } = json.metadata;
-  return { tweetId, tweetCreatorUserId, tweetCreatorUsername };
+  const prefix = "http://127.0.0.1:8080/ipfs"
+  const endpoint = `${prefix}/${cid}`
+
+  const gateways = [
+    endpoint, 
+    uri,
+    everlandPath,
+    classicPath,
+    dwebPath,
+    pinataPath,
+    infuraPath,
+    nftStoragePath,
+    web3Path,
+    fleekPath,
+    thirdPath,
+  ];
+
+  return firstSuccessful(
+    gateways.map((url) => async () => {
+      const res = await fetch(url);
+      if (!res.ok)
+        throw new Error(
+          `IPFS fetch failed: ${res.status} ${res.statusText} for ${url}`
+        );
+      const json: PeekJson = await res.json();
+      const { tweetId, tweetCreatorUserId, tweetCreatorUsername } =
+        json.metadata;
+      return { tweetId, tweetCreatorUserId, tweetCreatorUsername };
+    })
+  );
 }
